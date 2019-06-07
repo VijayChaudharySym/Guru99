593c1d65d176bd83291c32d83e9d005f5f621356
VijayChaudharySym <34708938+VijayChaudharySym@users.noreply.github.com>
2019-06-07 16:16:50 +0530

    Update AppTest.java

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -4,14 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class AppTest {
 	
 	protected WebDriver driver;
 	@Test
   public void guru99tutorials() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-		        "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setPlatform(Platform.LINUX);
+		//driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), caps);
+		driver = new RemoteWebDriver(new URL("http://redzonetest2.symc.symantec.com/wd/hub"), caps);
         String eTitle = "Meet Guru99";
         String aTitle = "";
         // launch Firefox and redirect it to the Base URL
@@ -31,4 +43,3 @@ public class AppTest {
 		
 	}
 }
-
